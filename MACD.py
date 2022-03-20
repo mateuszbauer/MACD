850cce24b124fd9b1cd8650f84a6d448d934e414
mateuszbauer <mateusz.bauer@intel.com>
2022-03-20 20:51:50 +0100

    Don't append zeros
    Signed-off-by: mateuszbauer <mateusz.bauer@intel.com>

--- MACD.py
+++ MACD.py
@@ -10,20 +10,18 @@ class MACD:
         self.stock_prices = self.df.Close.to_numpy()
         self.macd = self.calculate_ema(self.stock_prices, 12) - self.calculate_ema(self.stock_prices, 26)
         self.signal = self.calculate_ema(self.macd, 9)
-        
+
     def calculate_ema(self, arr, n):
         alpha = 2 / (n+1)
         ema = np.zeros(len(arr))
-        
+
         for index in range(0, len(arr)):
             # (1, n+1) matrix with values from current and n previous days
-            # if there's no data from previous days, fill with zeros
             values = np.array([arr[index:index+n+1]])    
-            values = np.array([np.append(values, [[0] * (n-values.size+1)])])
             
             # (n+1, 1) matrix with weights
             # w[i] = (1-alpha)^i, i <- 0..n+1
-            weights = np.array([[(1.0-alpha)**i] for i in range(n+1)])
+            weights = np.array([[(1.0-alpha)**i] for i in range(values.size)])
             
             ema[index] = (np.matmul(values, weights).item() / weights.sum())
             
